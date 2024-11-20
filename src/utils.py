60a59dfd8dfc565a11171ebc115240ef756412bd
Palak Jagwani <jagwanipalak02@gmail.com>
2024-11-20 21:32:30 +0530

    Hyper parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -22,21 +22,23 @@ def save_object_as_pklfile(file_path, obj):
 
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            params = param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,params,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
